53fa50cac04da2d8fa2d1ad0b39ef07f10a7d2c3
timoteobentes <bentestimoteo@gmail.com>
2021-11-25 22:44:21 -0400

    gospel+

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function redirecionar() {
+    window.open("https://www.youtube.com/watch?v=Xn2-kLe7tRc");
+}
+
+function clicou() {
+    window.open("https://www.themoviedb.org/movie/249660-god-s-not-dead/cast");
+}
+
+function serie() {
+    alert("Sem conteúdo!");
+}
+
+function doc() {
+    alert("Sem conteúdo!");
+}
